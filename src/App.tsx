ecdcc035086b6fcac65e030ddd6d517f2eeeeb57
Igor Alyanoy <igoralyanoy@gmail.com>
2022-05-04 12:44:55 +0300

    feat: add basic routing and styled dependecies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Board } from './pages/Board';
+import { MainBoards } from './pages/MainBoards';
+import { Welcome } from './pages/Welcome';
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/main" element={<MainBoards />} />
+      <Route path="/board" element={<Board />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/MainBoards.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const MainBoards = () => {
+  return <div>MainBoards</div>;
+};
