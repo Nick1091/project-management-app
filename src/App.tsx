ceac606af8ca7a4f04d5ba3f66383b6732d0b421
voroshilova <voroshilova.av@yandex.ru>
2022-05-04 21:40:03 +0400

    feat: add EditProfile and NotFound components, routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout';
 import { Board } from './pages/Board';
+import { EditProfile } from './pages/EditProfile';
 import { MainBoards } from './pages/MainBoards';
+import { NotFound } from './pages/NotFound';
 import { Welcome } from './pages/Welcome';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Route index element={<Welcome />} />
         <Route path="/main" element={<MainBoards />} />
         <Route path="/board" element={<Board />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
