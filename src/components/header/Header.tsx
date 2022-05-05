c62efb3bda49d9a115f7c91416ee0c2703896383
voroshilova <voroshilova.av@yandex.ru>
2022-05-05 18:09:15 +0400

    refactor: move code snippets to GuestBar and UserBar components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,11 @@
-import { AppBar, Box, Button, Container, IconButton, Link, Toolbar } from '@mui/material';
+import { AppBar, Container, Toolbar } from '@mui/material';
 import AppRegistrationOutlinedIcon from '@mui/icons-material/AppRegistrationOutlined';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import AddToQueueIcon from '@mui/icons-material/AddToQueue';
-import { Link as RouterLink } from 'react-router-dom';
+
 import { useEffect, useState } from 'react';
 
-import { LocalizationToggler } from '../localizationTogger/LocalizationTogger';
 import { Typography } from './Header-styles';
+import { UserBar } from '../userBar/UserBar';
+import { GuestBar } from '../guestBar/GuestBar';
 
 export const Header = () => {
   //this is for checking until authorization functionality is implemented
@@ -33,34 +32,7 @@ export const Header = () => {
           <Toolbar>
             <AppRegistrationOutlinedIcon sx={{ fontSize: 30 }}></AppRegistrationOutlinedIcon>
             <Typography>Project Management App</Typography>
-            {token ? (
-              <>
-                <LocalizationToggler />
-                <IconButton color="inherit">
-                  <AddToQueueIcon sx={{ fontSize: 30, mr: '15px' }}></AddToQueueIcon>
-                </IconButton>
-                <IconButton color="inherit">
-                  <Link color="inherit" component={RouterLink} to="/edit-profile">
-                    <ManageAccountsIcon sx={{ fontSize: 30, mr: '15px' }}></ManageAccountsIcon>
-                  </Link>
-                </IconButton>
-                <Button color="secondary" variant="contained">
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <>
-                <LocalizationToggler />
-                <Box mr={3}>
-                  <Button color="inherit" variant="outlined">
-                    Log In
-                  </Button>
-                </Box>
-                <Button color="secondary" variant="contained">
-                  Sign up
-                </Button>
-              </>
-            )}
+            {token ? <UserBar /> : <GuestBar />}
           </Toolbar>
         </Container>
       </AppBar>
