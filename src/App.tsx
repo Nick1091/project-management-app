e513c9e05868a8879043d887f72d210f381e7cf7
voroshilova <voroshilova.av@yandex.ru>
2022-05-05 17:22:09 +0400

    refactor: remove unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout';
 import { Board } from './pages/Board';
--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const EditProfile = () => {
   return <div>EditProfile</div>;
 };
