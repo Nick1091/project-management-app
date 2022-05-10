57a348cac4087111b53d28938bcf2d9d77ae7238
Igor Alyanoy <igoralyanoy@gmail.com>
2022-05-10 20:36:48 +0300

    refactor: update auth folder name

---
+++ src/components/Auth/SubComponentPassword.tsx
@@ -0,0 +1,33 @@
+import { Controller, useFormContext } from 'react-hook-form';
+import { TextField } from '@mui/material';
+import { passwordValidation } from '../../validation';
+
+export const SubComponentPassword = () => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name="password"
+      rules={passwordValidation}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          variant="outlined"
+          label="password"
+          type="password"
+          size="medium"
+          margin="dense"
+          fullWidth={true}
+          onChange={field.onChange}
+          value={field.value}
+          error={!!errors.password?.message}
+          helperText={errors.password?.message ?? ' '}
+        />
+      )}
+    />
+  );
+};
