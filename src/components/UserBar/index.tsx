8db48a0c0de933d3f6d69da6d78bd28f0cbc53da
voroshilova <voroshilova.av@yandex.ru>
2022-05-13 01:14:38 +0400

    feat: add "Go to Main Page" button with redirect

--- src/components/UserBar/index.tsx
+++ src/components/UserBar/index.tsx
@@ -16,6 +16,11 @@ export const UserBar = () => {
           <ManageAccountsIcon sx={{ fontSize: 30, mr: '15px' }}></ManageAccountsIcon>
         </Link>
       </IconButton>
+      <Link color="inherit" style={{ textDecoration: 'none' }} component={RouterLink} to="/main">
+        <Button color="inherit" variant="outlined" sx={{ mr: '15px' }}>
+          Go to Main Page
+        </Button>
+      </Link>
       <Button color="secondary" variant="contained">
         Sign Out
       </Button>
