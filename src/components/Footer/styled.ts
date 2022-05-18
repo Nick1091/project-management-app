2fccdbe61a65d4d0c5abb4d980ceaa160eef459c
voroshilova <voroshilova.av@yandex.ru>
2022-05-18 15:42:16 +0400

    refactor: remove margin from footer

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.div`
-  margin-top: 10px;
   width: 100%;
   height: 72px;
   box-shadow: 0px -7px 25px 0px rgba(34, 60, 80, 0.2);
@@ -12,13 +11,12 @@ export const DevsContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   max-width: 1900px;
-  height: 72px;
+  height: 80px;
   width: 100%;
-  margin: 0 auto;
   padding-left: 34px;
   padding-right: 34px;
 `;
 
 export const DevContainer = styled.span`
-  margin: 0 1em;
+  margin: 0 16px;
 `;
